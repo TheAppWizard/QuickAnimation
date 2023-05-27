68d212c41df78b6c902d7c7b04a980ec55f626d6
TheAppWizard <shreyasbunity@gmail.com>
2023-05-27 20:44:14 +0530

    Initial Commit

---
+++ QuickAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuickAnimation
+//
+//  Created by Shreyas Vilaschandra Bhike on 27/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickAnimation/QuickAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickAnimationApp.swift
+//  QuickAnimation
+//
+//  Created by Shreyas Vilaschandra Bhike on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
