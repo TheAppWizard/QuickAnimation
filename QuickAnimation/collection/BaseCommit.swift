1a2deccd511861c124cc2a93db7a42d7955df35e
TheAppWizard <shreyasbunity@gmail.com>
2023-08-06 19:28:23 +0530

    - Base Commit

---
+++ QuickAnimation/collection/BaseCommit.swift
@@ -0,0 +1,20 @@
+//
+//  BaseCommit.swift
+//  QuickAnimation
+//
+//  Created by Shreyas Vilaschandra Bhike on 06/08/23.
+//
+
+import SwiftUI
+
+struct BaseCommit: View {
+    var body: some View {
+        Text("Hello, Github!")
+    }
+}
+
+struct BaseCommit_Previews: PreviewProvider {
+    static var previews: some View {
+        BaseCommit()
+    }
+}
